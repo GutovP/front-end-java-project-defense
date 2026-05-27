78100acca696c3ecff93145146220f9ac30de5f1
Petar Gutov <pgutov@gmail.com>
2026-05-27 19:28:59 +0200

    basket component updated to signals

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,7 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { BasketService } from './basket.service';
 import { CommonModule } from '@angular/common';
-import { Basket } from '../core/models/basket';
 import { UserService } from '../user/user.service';
 import { ToastService } from '../core/toast/toast.service';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -13,35 +12,22 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './basket.component.html',
   styleUrl: './basket.component.css',
 })
-export class BasketComponent {
+export class BasketComponent implements OnInit {
   private basketService = inject(BasketService);
   private userService = inject(UserService);
   private toastService = inject(ToastService);
   private router = inject(Router);
 
-  basket: Basket | undefined | null;
+  basket = this.basketService.basket;
+  readonly isAdmin = computed(() => this.userService.getUserRole() === 'ADMIN');
 
-  get isAdmin() {
-    return this.userService.getUserRole() === 'ADMIN';
-  }
   ngOnInit(): void {
-    if(this.isAdmin) {
-      this.router.navigateByUrl('/products/all');
-    }
-    
     this.viewBasket();
   }
 
   viewBasket(): void {
     this.basketService.viewBasket().subscribe({
-      next: (data) => {
-        this.basket = data;
-
-        if (!this.basket.items || this.basket.items.length === 0) {
-          this.basket = null;
-        }
-      },
-      error: (err) => {
+      error: (err: HttpErrorResponse) => {
         if (err.status === 401) {
           this.userService.logout();
           this.toastService.activate('Please login to view your basket');
@@ -53,19 +39,15 @@ export class BasketComponent {
   }
 
   updateQuantity(productId: string, newQuantity: number): void {
-
-    this.basketService
-      .updateItemQuantity(productId, newQuantity)
-      .subscribe({
-        next: () => {
-          this.viewBasket();
-        },
-
-        error: (err: HttpErrorResponse) => {
-          this.toastService.activate(err.error.message);
-        },
-      });
+    this.basketService.updateItemQuantity(productId, newQuantity).subscribe({
+      error: (err: HttpErrorResponse) => {
+        this.toastService.activate(
+          err.error.message || 'Could not update quantity',
+        );
+      },
+    });
   }
+
   increaseQuantity(productId: string, currentQuantity: number): void {
     this.updateQuantity(productId, currentQuantity + 1);
   }
@@ -74,14 +56,10 @@ export class BasketComponent {
       this.updateQuantity(productId, currentQuantity - 1);
     }
   }
-  removeFromBasket(basketItemId: string) {
-
+  removeFromBasket(basketItemId: string): void {
     this.basketService.removeFromBasket(basketItemId).subscribe({
-      next: (response) => {
-        this.basket = response;
-      },
       error: (err: HttpErrorResponse) => {
-        this.toastService.activate(err.error.message);
+        this.toastService.activate(err.error.message || 'Could not remove item');
       },
     });
   }
