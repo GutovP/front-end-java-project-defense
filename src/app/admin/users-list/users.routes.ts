743f1e07a2ead19f9e49357830e9d7a5266ecb69
Petar Gutov <pgutov@gmail.com>
2026-05-28 17:39:14 +0200

    user routes guard changed

--- src/app/admin/users-list/users.routes.ts
+++ src/app/admin/users-list/users.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { AuthActivate } from '../../shared/guards/auth.guard';
+import { AdminActivate } from '../../shared/guards/admin.guard';
 
 export const USERS_ROUTES: Routes = [
   {
     path: '',
-    canActivate: [AuthActivate],
+    canActivate: [AdminActivate],
     loadComponent: () =>
       import('./users-list.component').then((m) => m.UsersListComponent),
   },
