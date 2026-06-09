8cf76e488dc4f64bd383ed1cc48e145f64c180b4
Petar Gutov <pgutov@gmail.com>
2026-06-09 18:45:48 +0200

    refactor(all-products): migrate to signals

--- src/app/admin/users-list/users-list.component.ts
+++ src/app/admin/users-list/users-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { User } from '../../core/models/user';
 import { AdminService } from '../admin.service';
 import { CommonModule } from '@angular/common';
--- src/app/product/all-products/all-products.component.ts
+++ src/app/product/all-products/all-products.component.ts
@@ -1,6 +1,5 @@
-
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Product } from '../../core/models/product';
 import { ProductService } from '../product.service';
@@ -10,30 +9,29 @@ import { UserService } from '../../user/user.service';
   selector: 'app-all-products',
   imports: [RouterLink],
   templateUrl: './all-products.component.html',
-  styleUrl: './all-products.component.css'
+  styleUrl: './all-products.component.css',
 })
 export class AllProductsComponent {
+  private productService = inject(ProductService);
+  private userService = inject(UserService);
+  private router = inject(Router);
 
-  products: Product[] | undefined;
-    private productService = inject(ProductService);
-    private userService = inject(UserService);
-    private router = inject(Router);
-  
-    ngOnInit(): void {
-      this.getAllProducts();
-    }
-  
-    getAllProducts() {
-      return this.productService.getProducts().subscribe({
-        next: (products) => {
-          this.products = products;
-        },
-        error: (error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            this.userService.logout();
-            this.router.navigate(['']);
-          }
-        },
-      });
-    }
+  readonly products = signal<Product[]>([]);
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    return this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
+      },
+      error: (error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.userService.logout();
+          this.router.navigate(['']);
+        }
+      },
+    });
+  }
 }
