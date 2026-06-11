56382fcdd11e42f2850c76fbbac0b0cacfeb0f82
Petar Gutov <pgutov@gmail.com>
2026-06-11 12:01:17 +0200

    loading spinner binded to the global app template

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ToastComponent } from './toast/toast.component';
+import { LoadingSpinnerComponent } from './global-loader/loading-spinner/loading-spinner.component';
 
 @NgModule({
   declarations: [],
-  imports: [HeaderComponent, ToastComponent],
-  exports: [HeaderComponent, ToastComponent],
+  imports: [HeaderComponent, ToastComponent, LoadingSpinnerComponent],
+  exports: [HeaderComponent, ToastComponent, LoadingSpinnerComponent],
 })
 export class CoreModule {}
